d623c0fbaf20478e5b8e9c9261e31c34bc98b6df
luannfirme <luannfirme@gmail.com>
2023-12-15 16:35:58 -0300

    update

--- RoadMapApp/Program.cs
+++
@@ -1,9 +0,0 @@
-using RoadMapApp;
-
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/pauloricardo", () => PauloRicardo.View());
-
-app.Run();
