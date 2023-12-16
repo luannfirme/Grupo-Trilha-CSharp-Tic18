6c8edd1a5e23a1c9b696ce3b99df10e4a1c74265
Luann Firme <luann.firme@daten.com.br>
2023-12-16 14:39:10 -0300

    Finalização

--- RoadMapApp/Program.cs
+++ RoadMapApp/Program.cs
@@ -5,6 +5,5 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/anacaroline", () => AnaCaroline.View());
 app.MapGet("/luannfirme", () => LuannFirme.View());
 app.Run();
