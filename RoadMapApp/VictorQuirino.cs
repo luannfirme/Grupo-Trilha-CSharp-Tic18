dde29e675a67d1490eb017072247eb95a5a33f9c
Víctor Quirino <victorsouzaq@gmail.com>
2023-12-18 17:14:28 -0300

    Implementando endpoint de skills do roadmap

---
+++ RoadMapApp/VictorQuirino.cs
@@ -0,0 +1,31 @@
+using System.Text;
+
+namespace RoadMapApp;
+public static class VictorQuirino
+{
+    public static string Name => "Víctor Souza Moreira Quirino";
+    public static List<(string, int)> Skills => new List<(string, int)>{
+        ("Fundamentos de C#", 3),
+        ("Habilidades Gerais de Desenvolvimento", 4),
+        ("Fundamentos de Bancos de Dados", 4),
+        ("ORM", 2),
+        ("Injeção de Dependência", 2),
+        ("Bancos de Dados", 3),
+        ("Micro-Serviços", 1),
+        ("Testes", 2)
+    };
+
+    public static string View(){
+        var sb = new StringBuilder();
+        sb.AppendLine($"Nome: {Name}");
+        sb.AppendLine();
+        sb.AppendLine("Habilidades:");
+            foreach (var skill in Skills){
+                sb.AppendLine($"\t{skill.Item1} - {skill.Item2} estrelas.");
+            }
+            var sum = Skills.Sum(x => x.Item2);
+            sb.AppendLine();
+            sb.AppendLine($"Total de estrelas: {sum}");
+            return sb.ToString();
+    }
+}
