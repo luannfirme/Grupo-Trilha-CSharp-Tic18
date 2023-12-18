d3323f92174edc663ab03dff37328d9fbf7a8725
Carol <carolnerisfonseca@gmail.com>
2023-12-17 23:55:59 -0300

    incluindo Anacaroline ao roadmap

--- RoadMapApp/Program.cs
+++ RoadMapApp/Program.cs
@@ -6,6 +6,7 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/luannfirme", () => LuannFirme.View());
 app.MapGet("/pauloricardo", () => PauloRicardo.View());
+app.MapGet("/anacaroline", () => AnaCaroline.View());
 app.MapGet("/caio", () => Caio.View());
 
 app.Run();
