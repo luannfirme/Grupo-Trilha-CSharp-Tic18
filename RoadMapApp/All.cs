38fd3f874bc22860912c70993e1772bcec8baa94
Luann Firme <luann.firme@daten.com.br>
2023-12-19 08:40:27 -0300

    limpeza e ajuste de espaçamento no código

--- RoadMapApp/All.cs
+++ RoadMapApp/All.cs
@@ -5,13 +5,6 @@ namespace RoadMapApp;
 
 public static class All
 {
-      // public static string View(){
-      //             var sb = new StringBuilder();
-      //             sb.AppendLine();
-      //             sb.AppendLine($"Soma geral da Equipe: /*+  LuannFirme.allStars()*/");
-      //             return sb.ToString();
-      //       }
-
       private static readonly List<List<(string, int)>> UserSkills = new List<List<(string, int)>>
       {
             AnaCaroline.Skills,
@@ -23,29 +16,31 @@ public static class All
 
       public static List<(string, int)> GetGroupSkills()
       {
-      return UserSkills.SelectMany(userSkills => userSkills)
-                        .GroupBy(skill => skill.Item1)
-                        .Select(groupedSkills => (groupedSkills.Key, groupedSkills.Sum(skill => skill.Item2)))
-                        .ToList();
+            return UserSkills.SelectMany(userSkills => userSkills)
+                              .GroupBy(skill => skill.Item1)
+                              .Select(groupedSkills => (groupedSkills.Key, groupedSkills
+                              .Sum(skill => skill.Item2)))
+                              .ToList();
       }
 
       public static int GetTotalStars()
       {
-            return UserSkills.SelectMany(userSkills => userSkills).Sum(skill => skill.Item2);
+            return UserSkills.SelectMany(userSkills => userSkills)
+                              .Sum(skill => skill.Item2);
       }
 
       public static string View()
-    {
-        var sb = new StringBuilder();
-        sb.AppendLine("Resumo de habilidades da equipe:");
-        sb.AppendLine();
-        sb.AppendLine("Habilidades:");
-        foreach (var skill in GetGroupSkills())
-        {
-            sb.AppendLine($"\t{skill.Item1} - {skill.Item2} estrelas");
-        }
-        sb.AppendLine();
-        sb.AppendLine($"Total de estrelas: {GetTotalStars()}");
-        return sb.ToString();
-    }
+      {
+            var sb = new StringBuilder();
+            sb.AppendLine("Resumo de habilidades da equipe:");
+            sb.AppendLine();
+            sb.AppendLine("Habilidades:");
+            foreach (var skill in GetGroupSkills())
+            {
+                  sb.AppendLine($"\t{skill.Item1} - {skill.Item2} estrelas");
+            }
+            sb.AppendLine();
+            sb.AppendLine($"Total de estrelas: {GetTotalStars()}");
+            return sb.ToString();
+      }
 }
